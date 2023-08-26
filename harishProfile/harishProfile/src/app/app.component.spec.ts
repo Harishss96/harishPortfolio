3d5cb5f8e638e3aa9519aedf922a8ceba64d0e0e
Harishss96 <harishss9650@gmail.com>
2023-08-27 00:43:42 +0530

    Port 1Commit

--- harishProfile/harishProfile/src/app/app.component.spec.ts
+++ harishProfile/harishProfile/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'harishProfiles'`, () => {
+  it(`should have as title 'harishProfile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('harishProfiles');
+    expect(app.title).toEqual('harishProfile');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('harishProfiles app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('harishProfile app is running!');
   });
 });
